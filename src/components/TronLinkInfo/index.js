f39b3519ed62dba4721858c04d79526525ee3130
Ujwal Battar <ujwal.battar@gmail.com>
2019-02-13 12:57:05 -0800

    set up TronLink account information component

--- src/components/TronLinkInfo/index.js
+++ src/components/TronLinkInfo/index.js
@@ -1,14 +1,74 @@
 import React, { Component } from "react";
 
+import "./TronLinkInfo.scss";
+
 export default class TronLinkInfo extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      accountAddress: "account address will show up here",
+      accountBalance: "account balance will show up here",
+      accountBandwidth: "account bandwidth will show up here"
+    };
   }
 
+  // Uncomment each function below then uncomment each call,
+  // one at a time to see your account information filled out
+  componentDidMount() {
+    // this.fetchAccountAddress();
+    // this.fetchAccountBalance();
+    // this.fetchAccountBandwidth();
+  }
+
+  // The function below will return an object with address, balance, create_time,
+  // account_resource
+  // async fetchAccountAddress() {
+  //   const account = await window.tronWeb.trx.getAccount();
+  //   const accountAddress = account.address; // Hex
+  //   // const accountAddressInASCii = window.tronWeb.address.fromHex(
+  //   //   accountAddress
+  //   // ); // ASCii
+  //
+  //   this.setState({
+  //     accountAddress: accountAddress
+  //   });
+  // }
+
+  // The function below will return the account balance in SUN as a number
+  // async fetchAccountBalance() {
+  //   const balanceInSun = await window.tronWeb.trx.getBalance(); //number
+  //   // const balanceInTRX = window.tronWeb.fromSun(balanceInSun); //string
+  //   // const changeBackToSun = window.tronWeb.toSun(balanceInTRX); //string
+  //
+  //   this.setState({
+  //     accountBalance: balanceInSun
+  //   });
+  // }
+
+  // The function below will return the account bandwidth as a number
+  // async fetchAccountBandwidth() {
+  //   const accountBandwidth = await window.tronWeb.trx.getBandwidth(); // number
+  //
+  //   this.setState({
+  //     accountBandwidth: accountBandwidth
+  //   });
+  // }
+
   render() {
+    const { accountAddress, accountBalance, accountBandwidth } = this.state;
     return (
-      <div>
-        <p>This is the TronLink Account Information Component </p>
+      <div className="tronLinkInfo-component-container">
+        <div className="account-info-header"> Account Information </div>
+        <div className="account-info-address">
+          Address: <span>{accountAddress}</span>
+        </div>
+        <div className="account-info-balance">
+          Balance: <span>{accountBalance}</span>
+        </div>
+        <div className="account-info-bandwidth">
+          Bandwidth: <span>{accountBandwidth}</span>
+        </div>
       </div>
     );
   }
